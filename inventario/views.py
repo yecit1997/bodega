67546d87846a2ed0b419c9e7d641def37a51d599
yecit1997 <ttorresyecit@gmail.com>
2024-09-06 18:56:11 -0500

    Se integran las alertas animadas

--- inventario/views.py
+++ inventario/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from producto.models import Productos, Estanteria, Tipo
-from django.db.models import Count, Sum
+from django.db.models import Sum
 
 
 @login_required()
 def inventario(request):
-    total_productos_estanteria = Estanteria.objects.annotate(total_productos=Count('productos'))
-    total_productos_estanteria = Estanteria.objects.annotate(total_cantidad=Sum('productos__cantidad'))
+   total_productos_categoria = Tipo.objects.annotate(total_productos=Sum('productos__cantidad'))
+   total_productos_estanteria = Estanteria.objects.annotate(total_cantidad=Sum('productos__cantidad'))   
+   distribucion = {}
+   for estante in Estanteria.objects.all():
+       distribucion[estante.codigo] = {}
+       for producto in Productos.objects.filter(ubicacion_estanteria=estante):
+           tipo = producto.tipo.tipo
+           if tipo not in distribucion[estante.codigo]:
+               distribucion[estante.codigo][tipo] = 0
+           distribucion[estante.codigo][tipo] += producto.cantidad 
 
-    distribucion = {}
-    for estante in Estanteria.objects.all():
-        distribucion[estante.codigo] = {}
-        for producto in Productos.objects.filter(ubicacion_estanteria=estante):
-            tipo = producto.tipo.tipo
-            if tipo not in distribucion[estante.codigo]:
-                distribucion[estante.codigo][tipo] = 0
-            distribucion[estante.codigo][tipo] += producto.cantidad 
-    
-    context = {
-       'distribucion':distribucion,
-       'total_productos_estanteria':total_productos_estanteria
-    }
-    return render(request, 'inventario/inventario.html', context=context)
+ 
+   context = {
+      'distribucion':distribucion,
+      'total_productos_estanteria':total_productos_estanteria.order_by('codigo'),
+      'total_productos_categoria':total_productos_categoria.order_by('tipo')
+   }
+   return render(request, 'inventario/inventario.html', context=context)
     
